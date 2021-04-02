0c9b22e0cc22189f77698b9755c9e549e28c65f4
Vignesh Iyer <vigneshpriyer@gmail.com>
2021-04-02 13:45:12 +0530

    Calculator Code

---
+++ calc.py
@@ -0,0 +1,33 @@
+def add(x,y):
+	return x+y
+
+def subtract(x,y):
+	return x-y
+
+def multiply(x,y):
+	return x*y
+
+def divide(x,y):
+	return x/y
+
+while(True):
+	p=input().split()
+	op=p[0]
+	if op == 'q':
+		break
+	x=float(p[1])
+	y=float(p[2])
+
+	if op=='add' or op=='a':
+		print(add(x,y))
+
+	if op=='subtract' or op=='s':
+		print(subtract(x,y))
+
+	if op=='multiply'  or op=='m':
+		print(multiply(x,y))
+
+	if op=='divide' or op=='d':
+		print(divide(x,y))
+
+
